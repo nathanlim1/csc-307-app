69a91300fe533410363a4b04d5280a842724c9b3
nathanlim1 <nlim10@calpoly.edu>
2024-10-04 03:09:22 -0700

    Added part of the input form

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -13,38 +13,38 @@ function TableHeader() {
     );
   }
   
-  function TableBody(props) {
-    const rows = props.characterData.map((row, index) => {
-      return (
-        <tr key={index}>
-            <td>{row.name}</td>
-            <td>{row.job}</td>
-            <td>
-            <button onClick={() => props.removeCharacter(index)}>
-                Delete
-            </button>
-            </td>
-        </tr>
-      );
-     }
-    );
+function TableBody(props) {
+const rows = props.characterData.map((row, index) => {
     return (
-        <tbody>
-          {rows}
-         </tbody>
-     );
-  }
+    <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+        <button onClick={() => props.removeCharacter(index)}>
+            Delete
+        </button>
+        </td>
+    </tr>
+    );
+    }
+);
+return (
+    <tbody>
+        {rows}
+        </tbody>
+    );
+}
   
-  function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody
-          characterData={props.characterData}
-          removeCharacter={props.removeCharacter}
-        />
-      </table>
-    );
-  }
+function Table(props) {
+return (
+    <table>
+    <TableHeader />
+    <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+    />
+    </table>
+);
+}
 
 export default Table;
